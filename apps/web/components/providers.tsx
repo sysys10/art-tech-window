60d4cc7fd70ccac396a31daeccd00fe5d9ca3b83
shinys10 <166797095+sysys10@users.noreply.github.com>
2025-08-01 17:00:00 +0900

    Update providers.tsx

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -1,18 +1,8 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import * as React from 'react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-      enableColorScheme
-    >
-      {children}
-    </NextThemesProvider>
-  )
+  return <>{children}</>
 }
